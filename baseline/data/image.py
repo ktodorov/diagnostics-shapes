7af73fb3e99d037604206f5effc2648b77d53f35
ktodorov <kztodorov@outlook.com>
2019-06-04 23:27:27 +0200

    Adds refactored baseline CEE model

---
+++ baseline/data/image.py
@@ -0,0 +1,7 @@
+class Image:
+    def __init__(self, shapes, colors, sizes, data, metadata):
+        self.shapes = shapes
+        self.colors = colors
+        self.sizes = sizes
+        self.data = data
+        self.metadata = metadata
