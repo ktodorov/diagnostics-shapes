183bb449cb2f244f4eed1421c1663a4719e7d395
ktodorov <kztodorov@outlook.com>
2019-08-15 11:57:11 +0200

    Adds train jobs

---
+++ jobs/train_multi_42.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --job-name=train_multi_42
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=3
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=24:00:00
+#SBATCH --mem=60000M
+#SBATCH --partition=gpu_shared_course
+#SBATCH --gres=gpu:1
+module purge
+module load eb
+module load Python/3.6.3-foss-2017b
+module load cuDNN/7.0.5-CUDA-9.0.176
+module load NCCL/2.0.5-CUDA-9.0.176
+export LD_LIBRARY_PATH=/hpc/eb/Debian9/cuDNN/7.1-CUDA-8.0.44-GCCcore-5.4.0/lib64:$LD_LIBRARY_PATH
+
+for alpha in 0.2 0.5 0.8
+do
+    srun python3 -u train_game.py --device cuda --seed 42 --iterations 30000 --multi-task --multi-task-lambda $alpha >> 'output/train-multi-seed-42-alpha-'$alpha'.out'
+done
